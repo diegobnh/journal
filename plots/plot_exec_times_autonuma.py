37b8708c84f54987504c83a0f4b5319febcc693f
diegobnh <37424347+diegobnh@users.noreply.github.com>
2022-09-08 15:36:03 -0300

    Update plot_exec_times_autonuma.py

--- plots/plot_exec_times_autonuma.py
+++ plots/plot_exec_times_autonuma.py
@@ -1,14 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("../exec_times.csv", names=["app_name", "Pressure_30", "Pressure_50", "Pressure_70"])
+df = pd.read_csv("../exec_times.csv", names=["app_name", "No_pressure", "Pressure_30", "Pressure_50", "Pressure_70"])
+
+df["Pressure_30"] = round(df["Pressure_30"]/df["No_pressure"], 2)
+df["Pressure_50"] = round(df["Pressure_50"]/df["No_pressure"], 2)
+df["Pressure_70"] = round(df["Pressure_70"]/df["No_pressure"], 2)
+
+df.drop("No_pressure", axis=1, inplace=True)
 df.set_index("app_name", inplace=True)
-df.plot(kind="bar")
 
-plt.xticks(rotation = 45)
+df.plot(kind="bar", figsize=(7, 3))
+
+plt.xticks(rotation = 60)
 plt.xlabel("Workloads")
-plt.ylabel("Execution Time (Min)")
-
+plt.ylabel("Slowdown Normalized by No Pressure")
+plt.axhline(1, color='black', linestyle='--')
 filename = "exec_time_autonuma_mem_pressure.pdf"
 plt.savefig(filename, bbox_inches="tight")
 plt.clf()
+
