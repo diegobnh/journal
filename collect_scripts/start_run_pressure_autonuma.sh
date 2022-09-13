4cc5d0dadefb4d3bba00c24972c28f202e112830
diegobnh <37424347+diegobnh@users.noreply.github.com>
2022-09-13 15:30:16 -0300

    Update start_run_pressure_autonuma.sh

--- collect_scripts/start_run_pressure_autonuma.sh
+++ collect_scripts/start_run_pressure_autonuma.sh
@@ -43,24 +43,3 @@ for ((i = 0; i < ${#TYPES_OF_MEM_PRESSURE[@]}; i++)); do
     done
 done
 
-
-: '
-#Static Mapping
-for ((j = 0; j < ${#APP_DATASET[@]}; j++)); do
-    echo "Running:"${APP_DATASET[$j]}
-
-    sudo -u dmoura mkdir -p ${APP_DATASET[$j]}/static_mapping
-    sudo -u dmoura chmod +777 ${APP_DATASET[$j]}/static_mapping
-
-    cd ${APP_DATASET[$j]}/static_mapping
-    rm -f *
-
-    cp ../../run.sh .
-    cp ../../shared_library/mmap_intercept_to_static_bind.so .
-
-    sudo ./run.sh ${APP[$j]} ${DATASET[$j]} static_mapping
-
-    rm run.sh *.so call_stack.txt
-    cd ../..
-done
-'
