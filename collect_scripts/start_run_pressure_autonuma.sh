f61d572fa99fe9c8104b2dfc60c7de823ffb7bff
diegobnh <37424347+diegobnh@users.noreply.github.com>
2022-09-13 15:29:35 -0300

    Update and rename start_run.sh to start_run_pressure_autonuma.sh

--- collect_scripts/start_run_pressure_autonuma.sh
+++ collect_scripts/start_run_pressure_autonuma.sh
@@ -4,27 +4,6 @@ source app_dataset.sh
 
 gcc -o lock_memory lock_memory.c
 
-#Autonuma - Default
-for ((j = 0; j < ${#APP_DATASET[@]}; j++)); do
-    echo "Running:"${APP_DATASET[$j]}
-
-    sudo -u dmoura mkdir -p ${APP_DATASET[$j]}/autonuma
-    sudo -u dmoura chmod +777 ${APP_DATASET[$j]}/autonuma
-
-    cd ${APP_DATASET[$j]}/autonuma
-    rm -f *
-
-    cp ../../run.sh .
-    cp ../../shared_library/mmap_intercept_only_to_trace.so .
-
-    sudo ./run.sh ${APP[$j]} ${DATASET[$j]} autonuma
-
-    rm run.sh *.so call_stack.txt
-    cd ../..
-done
-
-
-: '
 #Autonuma with Memory Pressure
 for ((i = 0; i < ${#TYPES_OF_MEM_PRESSURE[@]}; i++)); do
     for ((j = 0; j < ${#APP_DATASET[@]}; j++)); do
@@ -63,7 +42,7 @@ for ((i = 0; i < ${#TYPES_OF_MEM_PRESSURE[@]}; i++)); do
         sleep 10
     done
 done
-'
+
 
 : '
 #Static Mapping
