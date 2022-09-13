26fcc1770b202047e666e7574a3c4a3c66aaf22c
diegobnh <37424347+diegobnh@users.noreply.github.com>
2022-09-13 15:35:36 -0300

    Update start_post_process.sh

--- collect_scripts/start_post_process.sh
+++ collect_scripts/start_post_process.sh
@@ -2,6 +2,17 @@
 
 source app_dataset.sh
 
+for ((j = 0; j < ${#APP_DATASET[@]}; j++)); do
+    echo "Running:"${APP_DATASET[$j]}
+    
+    cd ${APP_DATASET[$j]}/autonuma
+    sudo ../../post_process.sh ${APP[$j]} ${APP_DATASET[$j]} > /dev/null 2>&1
+    python3 ../../mmap_break_to_chunks.py
+    python3 ../../munmap_break_to_chunks.py
+	
+    cd ../..
+done
+
 for ((i = 0; i < ${#TYPES_OF_MEM_PRESSURE[@]}; i++)); do
     for ((j = 0; j < ${#APP_DATASET[@]}; j++)); do
         echo "Running:"${APP_DATASET[$j]}" for memory pressure:"${TYPES_OF_MEM_PRESSURE[$i]} 
@@ -16,10 +27,7 @@ for ((i = 0; i < ${#TYPES_OF_MEM_PRESSURE[@]}; i++)); do
 
         cd ${APP_DATASET[$j]}/$folder_name
 
-        cp ../../post_process.sh .
-	sudo ./post_process.sh ${APP[$j]} ${APP_DATASET[$j]} > /dev/null 2>&1
-	rm post_process.sh
-        
+	sudo ../../post_process.sh ${APP[$j]} ${APP_DATASET[$j]} > /dev/null 2>&1
         python3 ../../mmap_break_to_chunks.py
         python3 ../../munmap_break_to_chunks.py
 
