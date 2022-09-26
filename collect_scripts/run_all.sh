831be909943a90941bfcaaf5fcb4f02960626713
diegobnh <37424347+diegobnh@users.noreply.github.com>
2022-09-26 14:42:00 -0300

    Update run_all.sh

--- collect_scripts/run_all.sh
+++ collect_scripts/run_all.sh
@@ -2,13 +2,8 @@
 #!/bin/bash
 
 sudo ./start_run_default_autonuma.sh
-
 ./generate_memory_pressures.sh
-
 sudo ./start_run_pressure_autonuma.sh
-
 sudo ./start_post_process.sh
-
 ./start_mapping.sh
-
-./start_static_mapping.sh
+sudo ./start_run_static.sh
