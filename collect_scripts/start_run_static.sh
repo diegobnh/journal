4616d98a5b5b4fa32fc2f015ea1de753cb9961ef
diegobnh <37424347+diegobnh@users.noreply.github.com>
2022-09-26 14:28:46 -0300

    Update start_run_static.sh

--- collect_scripts/start_run_static.sh
+++ collect_scripts/start_run_static.sh
@@ -12,13 +12,16 @@ for ((i = 0; i < ${#TYPES_OF_MEM_PRESSURE[@]}; i++)); do
     for ((j = 0; j < ${#APP_DATASET[@]}; j++)); do
         if [[ ${TYPES_OF_MEM_PRESSURE[$i]} == "30" ]]; then
             mem_press=${MEM_PRESSURE_30[$j]}
-            cp ../${APP_DATASET[$j]}/autonuma/"static_mapping_"$mem_press".txt" .
+            cp ../${APP_DATASET[$j]}/autonuma/"static_mapping_30.txt" static_mapping.txt
+            #cp static_mapping_30.txt static_mapping.txt
         elif [[ ${TYPES_OF_MEM_PRESSURE[$i]} == "50" ]]; then
             mem_press=${MEM_PRESSURE_50[$j]}
-            cp ../${APP_DATASET[$j]}/autonuma/"static_mapping_"$mem_press".txt" .
+            cp ../${APP_DATASET[$j]}/autonuma/"static_mapping_50.txt" static_mapping.txt
+            #cp static_mapping_30.txt static_mapping.txt
         else
             mem_press=${MEM_PRESSURE_70[$j]}
-            cp ../${APP_DATASET[$j]}/autonuma/"static_mapping_"$mem_press".txt" .
+            cp ../${APP_DATASET[$j]}/autonuma/"static_mapping_70.txt" static_mapping.txt
+            #cp static_mapping_30.txt static_mapping.txt
         fi
         echo "Collecting Static Mapping:"${APP_DATASET[$j]}, " Memory Pressure of "${TYPES_OF_MEM_PRESSURE[$i]}
   	    cp ../shared_library/mmap_intercept_to_static_bind.so .
@@ -37,9 +40,8 @@ for ((i = 0; i < ${#TYPES_OF_MEM_PRESSURE[@]}; i++)); do
         kill -10 $lock_memory_pid
 
         mkdir -p ${APP_DATASET[$j]}_${TYPES_OF_MEM_PRESSURE[$i]}
-        mv static_mapping_* static_out_*.csv track_info_${APP_DATASET[$j]}.csv ${APP_DATASET[$j]}_${TYPES_OF_MEM_PRESSURE[$i]}
+        mv static_mapping.txt static_out_*.csv track_info_${APP_DATASET[$j]}.csv ${APP_DATASET[$j]}_${TYPES_OF_MEM_PRESSURE[$i]}
         rm -f call_stack.txt perf.data
         sleep 10
     done
 done
-
