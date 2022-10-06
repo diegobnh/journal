f0586294d1aad6a1f2ead1840752bd427a3fbd28
diegobnh <37424347+diegobnh@users.noreply.github.com>
2022-10-06 18:19:52 -0300

    Update generate_static_mapping.sh

--- collect_scripts/generate_static_mapping.sh
+++ collect_scripts/generate_static_mapping.sh
@@ -3,7 +3,7 @@
 source app_dataset.sh
 
 for ((j = 0; j < ${#APP_DATASET[@]}; j++)); do
-    echo "Running:"${APP_DATASET[$j]}
+    echo "Generating chunks to static mapping:"${APP_DATASET[$j]}
 
     cd ${APP_DATASET[$j]}/autonuma
     python3 ../../generate_static_mapping.py
